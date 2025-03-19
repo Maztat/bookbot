d139f64ef43899aec7c474bf4bebacf69a2bcf5e
Maztat <maztatmail@gmail.com>
2025-03-19 05:43:34 -0500

    update main.py, update stats.py

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from stats import wordcount
+from stats import word_count, sorted_char_counts
+import sys
 
-def main():
-    book = "frankenstein.txt"
-    print(f"{wordcount(book)} words found in the document")
+def main(book):
+    if len(book) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+    else:
+        book = book[1]
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book}...")
+        print("----------- Word Count ----------")
+        print(f"Found {word_count(book)} total words")
+        print("--------- Character Count -------")
+        sorted_char_count = sorted_char_counts(book)
+        for key in sorted_char_count:
+            if key == "\n":
+                print("\\n: " + str(sorted_char_count[key]))
+            else:
+                print(str(key) + ": " + str(sorted_char_count[key]))
+        print("============= END ===============")
     return
 
-main()
+print(sys.argv)
+main(sys.argv)
--- stats.py
+++ stats.py
@@ -1,9 +1,31 @@
-def get_book_text(book):
-    path = "books/" + book
-    with open(path) as f:
+def get_book_text(book:str):
+    with open(book) as f:
         return f.read()
-    return
+    return NotADirectoryError
 
-def wordcount(book:str):
+def word_count(book:str):
     word_num = len(get_book_text(book).split())
     return word_num
+
+def char_counts(book:str):
+    char_count = dict()
+    book = get_book_text(book)
+    for letter in book:
+        letter = letter.lower()
+        if letter in char_count:
+            char_count[letter] += 1
+        else:
+            char_count[letter] = 1
+    return char_count
+
+def sorted_char_counts(book:str):
+    char_count = char_counts(book)
+    sorted_char_count = dict()
+    while len(sorted_char_count) < len(char_count):
+        biggest = str(list(char_count.keys())[0])
+        for key in char_count:
+            if not key in sorted_char_count:
+                if char_count[key] > char_count[biggest]:
+                    biggest = key
+        sorted_char_count[biggest] = char_count.pop(biggest)
+    return sorted_char_count
